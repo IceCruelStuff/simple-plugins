aa8fdd5ea4efa0f53560012a008a28364b2724e4
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-20 18:51:12 -0700

    Update GroundTask.php

--- DeadlyFlight/src/SOFe/DeadlyFlight/GroundTask.php
+++ DeadlyFlight/src/SOFe/DeadlyFlight/GroundTask.php
@@ -7,14 +7,16 @@ namespace SOFe\DeadlyFlight;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\scheduler\PluginTask;
 
-class GroundTask extends PluginTask{
-	public function onRun(int $currentTick) : void{
+class GroundTask extends PluginTask {
+
+	public function onRun(int $currentTick) : void {
 		$damage = (float) $this->getOwner()->getConfig()->get("damage", 1);
 		$minTime = (int) $this->getOwner()->getConfig()->get("min-flight-time", 30);
-		foreach($this->getOwner()->getServer()->getOnlinePlayers() as $player){
-			if(!$player->isOnGround() && $player->getInAirTicks() >= $minTime){
+		foreach ($this->getOwner()->getServer()->getOnlinePlayers() as $player) {
+			if (!$player->isOnGround() && $player->getInAirTicks() >= $minTime) {
 				$player->attack(new EntityDamageEvent($player, EntityDamageEvent::CAUSE_FALL, $damage));
 			}
 		}
 	}
+
 }
