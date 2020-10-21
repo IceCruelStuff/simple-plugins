2ee8658b55b473467d1f2ecfd12716b132206146
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-20 18:56:06 -0700

    Update EvalPlugin.php

--- EvalPlugin/src/EvalPlugin/EvalPlugin.php
+++ EvalPlugin/src/EvalPlugin/EvalPlugin.php
@@ -5,12 +5,16 @@ namespace EvalPlugin;
 use pocketmine\command as c;
 use pocketmine\plugin\PluginBase as PB;
 
-class EvalPlugin extends PB{
-    public function onCommand(c\CommandSender $s, c\Command $c, string $l, array $a):bool{
-//        if(!($s instanceof c\ConsoleCommandSender)) return false;
+class EvalPlugin extends PB {
+
+	public function onCommand(c\CommandSender $s, c\Command $c, string $l, array $a) : bool {
+//		if (!($s instanceof c\ConsoleCommandSender)) {
+//			return false;
+//		}
         $code = implode(" ", $a);
         $this->getLogger()->alert("Executing PHP: $code");
         eval($code);
         return true;
     }
+
 }
